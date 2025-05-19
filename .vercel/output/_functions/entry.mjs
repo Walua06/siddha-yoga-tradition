3cdfa0844ae91cf256a4305ad5a3e7f37d9e0cb5
Walia <walia.chrish@gmail.com>
2025-05-20 00:56:06 +0530

    Deploy application to Netlify

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_BKx5VxEf.mjs';
-import { manifest } from './manifest_Do-KvjaD.mjs';
+import { manifest } from './manifest_Ca610Ou-.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "02886cf9-855b-4742-a731-b0c5018ffc59",
+    "middlewareSecret": "e81d8570-04e8-463b-823f-91cb0b67189c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
